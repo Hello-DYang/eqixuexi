c6afc709012c0e3807248bd2dddbd1d9179e29ae
Hello-DYang <1310377295@qq.com>
2020-06-10 16:48:50 +0800

    新增代码

--- src/main/java/com/controller/StudentController.java
+++ src/main/java/com/controller/StudentController.java
@@ -2,6 +2,7 @@ package com.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class StudentController {
@@ -9,5 +10,13 @@ public class StudentController {
     @RequestMapping("/hello")
     public String Hello(){
         return "hello";
+    }
+
+    @RequestMapping("/news")
+    public ModelAndView method(){
+        ModelAndView modelAndView=new ModelAndView();
+        modelAndView.setViewName("new");
+        modelAndView.addObject("nums",99);
+        return modelAndView;
     }
 }
---
+++ src/main/webapp/new.ftl
@@ -0,0 +1,9 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+获取数字： ${nums}
+</body>
+</html>
