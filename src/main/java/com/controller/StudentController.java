464b868d419eb46d2b4a581687e4582ec2b268fe
Hello-DYang <1310377295@qq.com>
2020-06-12 16:09:53 +0800

    06-12更新代码

--- src/main/java/com/controller/StudentController.java
+++ src/main/java/com/controller/StudentController.java
@@ -1,11 +1,17 @@
 package com.controller;
 
+import com.service.StudentService;
+import com.service.studentServiceImpl.StudentServiceImpl;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class StudentController {
+    @Autowired
+    private StudentService studentService;
 
     @RequestMapping("/hello")
     public String Hello(){
@@ -19,4 +25,20 @@ public class StudentController {
         modelAndView.addObject("nums",99);
         return modelAndView;
     }
+
+
+    @CrossOrigin(origins = "*")
+//    @PostMapping("/validate")
+    @RequestMapping("/validate")
+    @ResponseBody
+    public String validate(@RequestParam String username, @RequestParam String pwd){
+        System.out.println(username+pwd);
+        boolean b=studentService.validateStudent(username,pwd);
+        if (b){
+            return "登陆成功！！";
+        }else {
+            return "登陆失败!!";
+        }
+    }
+
 }
